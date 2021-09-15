f7fa03416409b9dcf8daa74223601a5a5b148d36
ThuanPx <thuanpx1710@gmail.com>
2021-09-15 23:11:26 +0700

    fix gradle build library

--- KtExt/src/main/java/com/thuanpx/ktext/context/FragmentExt.kt
+++ KtExt/src/main/java/com/thuanpx/ktext/context/FragmentExt.kt
@@ -8,32 +8,12 @@ import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.thuanpx.ktext.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 /**
  * Created by ThuanPx on 3/15/20.
  */
 
-/**
- * Launches a new coroutine and repeats `block` every time the Fragment's viewLifecycleOwner
- * is in and out of `minActiveState` lifecycle state.*
- * Source: https://medium.com/androiddevelopers/repeatonlifecycle-api-design-story-8670d1a7d333
- */
-inline fun Fragment.launchAndRepeatWithViewLifecycle(
-    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
-    crossinline block: suspend CoroutineScope.() -> Unit
-) {
-    viewLifecycleOwner.lifecycleScope.launch {
-        viewLifecycleOwner.lifecycle.repeatOnLifecycle(minActiveState) {
-            block()
-        }
-    }
-}
 
 fun Fragment.replaceFragment(
     @IdRes containerId: Int,
